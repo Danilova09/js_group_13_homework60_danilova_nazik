467f994b37da35727524d98afd1a14ac923073a9
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-11-30 00:04:34 +0600

    Вывод компонентов с использованием сервиса roulette

--- roulette/src/app/app.component.ts
+++ roulette/src/app/app.component.ts
@@ -11,16 +11,48 @@ export class AppComponent implements OnInit {
   balance = 100;
   selectedCategory = 'red';
   numbers: number[] = [];
+  number!: number;
+  hiddenColor!: string;
+
 
   constructor(private rouletteService: RouletteService) {
-    this.numbers = rouletteService.getNumbersArray();
-    rouletteService.numbersArrayChange.subscribe((numbers: number[]) => {
-      this.numbers = numbers;
-    });
+
   }
 
   ngOnInit() {
-
+    this.numbers = this.rouletteService.getNumbersArray();
+    this.rouletteService.numbersArrayChange.subscribe((numbers: number[]) => {
+      this.numbers = numbers;
+    });
+    this.rouletteService.newNumber.subscribe((newNumber: number) => {
+      this.number = newNumber;
+      this.hiddenColor = this.rouletteService.getColor(this.number);
+      if (this.hiddenColor === 'zero' && this.selectedCategory === 'zero') {
+        this.balance += 35;
+      } else if (this.hiddenColor === this.selectedCategory) {
+        this.balance += parseInt(this.rate);
+      } else if (this.hiddenColor !== this.selectedCategory) {
+        if (this.balance === 0 || this.balance < 0) {
+          this.balance = 0;
+          this.stop();
+        } else {
+          this.balance -= parseInt(this.rate);
+        }
+      }
+    });
   }
 
+  start() {
+    this.rouletteService.start();
+  }
+
+  stop() {
+    this.rouletteService.stop();
+  }
+
+  reset() {
+    this.rouletteService.reset();
+    this.balance = 100;
+    this.rate = '1';
+  }
 }
--- roulette/src/app/shared/roulette.service.ts
+++ roulette/src/app/shared/roulette.service.ts
@@ -1,11 +1,38 @@
 import { EventEmitter } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 
 export class RouletteService {
   private numbers: number[] = [];
   numbersArrayChange = new EventEmitter<number[]>();
+  newNumber = new EventEmitter<number>();
+  private isInterval = false;
+  private startInterval!: Observable<number>;
+  private intervalSubscription!: Subscription;
 
 
+  start() {
+    this.startInterval = interval(1000);
+    if (!this.isInterval) {
+      this.intervalSubscription = this.startInterval.subscribe((id: number) => {
+        this.isInterval = true;
+        let randomNumber = this.generateNumber();
+        this.newNumber.emit(randomNumber);
+        this.numbers.push(randomNumber);
+        this.onNumbersArrayChange();
+      });
+    }
+  }
 
+  stop() {
+    this.intervalSubscription.unsubscribe();
+    this.isInterval = false;
+  }
+
+  reset() {
+    this.numbers = [];
+    this.onNumbersArrayChange();
+    this.stop();
+  }
 
   getNumbersArray() {
     return this.numbers.slice();
@@ -13,6 +40,12 @@ export class RouletteService {
 
   onNumbersArrayChange() {
     this.numbersArrayChange.emit(this.numbers);
+  }
+
+  generateNumber(min = 0, max = 36) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   getColor(number: number) {
